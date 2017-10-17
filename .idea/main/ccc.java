dffa72087122b5e8b8b822fa098b049ba18d966b
yanxuefei <1781800026@qq.com>
2017-10-17 22:51:57 +0800

    8888

---
+++ .idea/main/ccc.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/10/17.
+ */
+public class ccc {
+
+    public static void  mian (String []args){
+        System.out.println("你好啊,世界");
+        System.out.println("你好啊,世界");
+        System.out.println("你好啊,世界");
+        System.out.println("你好啊,世界");
+    }
+
+}
